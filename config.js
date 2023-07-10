95a2d01ff665cb3f10662abc372c53324f5a1479
kurniadikevin <kurniadikevin@gmail.com>
2023-07-10 21:01:04 +0700

    add port env

--- config.js
+++ config.js
@@ -7,7 +7,8 @@ config = {
         host     : process.env.MYSQLHOST,
         user     :  process.env.MYSQLUSER,
         password :  process.env.MYSQLPASSWORD,
-        database : process.env.MYSQLDATABASE
+        database : process.env.MYSQLDATABASE,
+        port : process.env.MYSQLPORT
     })
 };
 module.exports = config;
